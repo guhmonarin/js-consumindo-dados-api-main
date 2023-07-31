6118c2965baf2ce4b7c515f4f7133a55d691df00
Gustavo Monarin <gustavomonarin11@gmail.com>
2023-07-31 12:47:23 -0300

    atualizacao

---
+++ js/script.js
@@ -0,0 +1,10 @@
+consultarCep = fetch('https://viacep.com.br/ws/17700000/json')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro ){
+            throw Error('Esse cep não existe')
+        } else
+            console.log(r)
+        })
+    .catch(erro => console.log(erro))
+    .finally (mensagem => console.log('Processamento concluido'))
